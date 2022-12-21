f641714242ea4380caab7734be65dc6a5041f6fa
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2022-12-21 10:53:15 -0500

    filtering

--- main.py
+++ main.py
@@ -5,16 +5,53 @@ import matplotlib.pyplot as plt
 
 
 
-ip_address=[]
+# ip_address=[]
+
+# try:
+#     for i in log:
+#         ip_address.append(i.split(' ')[0])
+
+# except UnicodeDecodeError:
+#     print()
+
+
+# ip_address_counter= Counter(ip_address)
+
+# result = {key:value for (key, value) in ip_address_counter.items() if value > 500}
+# print(result)
+
+
+# ip_address_counter= Counter(result)
+
+# print(ip_address)
+
+# plt.bar(ip_address_counter.keys(), ip_address_counter.values())
+
+
+# plt.show()
+
+dates=[]
 
 try:
     for i in log:
-        print(i.split(' ')[0])
+        dates.append(i.split(' ')[3])
+        dates.append(i.split(':')[0])
 
 except UnicodeDecodeError:
     print()
 
 
-ip_address_counter= Counter(ip_address)
+dates_counter= Counter(dates)
+
+result = {key:value for (key, value) in dates_counter.items() if value > 500}
+print(result)
+
+
+dates_counter= Counter(result)
+
+print(dates)
+
+plt.bar(dates_counter.keys(), dates_counter.values())
+
 
 plt.show()
