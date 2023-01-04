d9559f91d38135d4c239fbace109efd2c1b31849
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2023-01-04 09:53:41 -0500

    .

--- main.py
+++ main.py
@@ -20,66 +20,67 @@ fig, ax = plt.subplots()
 
 # ip_address_counter= Counter(ip_address)
 
-# result = {key:value for (key, value) in ip_address_counter.items() if value > 500}
+# result = {key:value for (key, value) in ip_address_counter.items() if value > 850}
 # print(result)
-
+# bar_colors = ['tab:red', 'tab:blue', 'tab:green',]
 
 # ip_address_counter= Counter(result)
 
 # print(ip_address)
 
-# plt.bar(ip_address_counter.keys(), ip_address_counter.values())
-
-
-# plt.show()
-
-# dates=[]
-# plt.xlabel('Dates')
-# plt.ylabel('Average Rating')
-# plt.title('Barplot')
-
-
-# try:
-#     for i in log:
-#         dates.append(i.split(' ')[3])
-#         dates.append(i.split(':')[0])
-
-# except UnicodeDecodeError:
-#     print()
-
-
-# dates_counter= Counter(dates)
-
-# result = {key:value for (key, value) in dates_counter.items() if value > 500 }
-# print(result)
-
-
-# dates_counter= Counter(result)
-
-# print(dates)
-
-# plt.bar(dates_counter.keys(), dates_counter.values())
-
-
+# plt.bar(ip_address_counter.keys(), ip_address_counter.values(), color=bar_colors)
 
 # plt.show()
 
+dates=[]
+plt.xlabel('Dates')
+plt.ylabel('Average Rating')
+plt.title('Barplot')
+
+
+try:
+    for i in log:
+        dates.append(i.split(' ')[3])
+        dates.append(i.split(':')[0])
+
+except UnicodeDecodeError:
+    print()
 
 
 
 
+dates_counter= Counter(dates)
 
-fig, ax = plt.subplots()
+result = {key:value for (key, value) in dates_counter.items() if value > 500 }
+print(result)
 
-fruits = ['apple', 'blueberry', 'cherry', 'orange']
-counts = [40, 100, 30, 55]
-bar_labels = ['red', 'blue', '_red', 'orange']
-bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
 
-ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+dates_counter= Counter(result)
 
-ax.set_ylabel('fruit supply')
-ax.set_title('Fruit supply by kind and color')
-ax.legend(title='Fruit color')
+print(dates)
+
+plt.bar(dates_counter.keys(), dates_counter.values())
+
+
 
 plt.show()
+
+
+
+
+
+
+# fig, ax = plt.subplots()
+
+# fruits = ['apple', 'blueberry', 'cherry', 'orange']
+# counts = [40, 100, 30, 55]
+# bar_labels = ['red', 'blue', '_red', 'orange']
+# bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+# ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+# ax.set_ylabel('fruit supply')
+# ax.set_title('Fruit supply by kind and color')
+# ax.legend(title='Fruit color')
+
+# plt.show()
