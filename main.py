3ede6260ac65db6918c3c9d73907687d850f166e
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2023-01-05 10:43:40 -0500

    .

--- main.py
+++ main.py
@@ -4,83 +4,74 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 
-ip_address=[]
+# ip_address=[]
 
-plt.xlabel('IP Adressess')
-plt.ylabel('Average Rating')
-plt.title('Barplot')
-
-try:
-    for i in log:
-        ip_address.append(i.split(' ')[0])
-
-except UnicodeDecodeError:
-    print()
-
-
-ip_address_counter= Counter(ip_address)
-
-result = {key:value for (key, value) in ip_address_counter.items() if value > 850}
-print(result)
-bar_colors = ['tab:red', 'tab:blue', 'tab:green',]
-
-ip_address_counter= Counter(result)
-
-print(ip_address)
-
-plt.bar(ip_address_counter.keys(), ip_address_counter.values(), color=bar_colors)
-
-plt.show()
-
-# dates=[]
-# plt.xlabel('Dates')
-# plt.ylabel('Average Rating')
+# plt.xlabel('IP Adressess')
+# plt.ylabel('Frequency')
 # plt.title('Barplot')
-
 
 # try:
 #     for i in log:
-#         dates.append(i.split(' ')[3])
-#         dates.append(i.split(':')[0])
+#         ip_address.append(i.split(' ')[0])
 
 # except UnicodeDecodeError:
 #     print()
 
 
+# ip_address_counter= Counter(ip_address)
 
-
-# dates_counter= Counter(dates)
-
-# result = {key:value for (key, value) in dates_counter.items() if value > 500 }
+# result = {key:value for (key, value) in ip_address_counter.items() if value > 8000}
 # print(result)
+# bar_colors = ['tab:red', 'tab:blue', 'tab:green',]
+
+# ip_address_counter= Counter(result)
+
+# print(ip_address)
+
+# plt.bar(ip_address_counter.keys(), ip_address_counter.values(), color=bar_colors)
+
+# plt.show()
+
+dates=[]
+plt.xlabel('Dates')
+plt.ylabel('Visitors')
+plt.title('Barplot')
 
 
-# dates_counter= Counter(result)
+try:
+    for i in log:
+        dates.append(i.split(' ')[3].split(':')[0])
+
+except UnicodeDecodeError:
+    print(i)
+    print()
+
+
+
+
+dates_counter= Counter(dates)
+
+result = {key:value for (key, value) in dates_counter.items() if value > 500 }
+print(result)
+bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:pink']
+
+
+dates_counter= Counter(result)
 
 # print(dates)
 
-# plt.bar(dates_counter.keys(), dates_counter.values())
+plt.bar(dates_counter.keys(), dates_counter.values(),color=bar_colors, width=0.4)
+
+  
+
+plt.xticks(rotation=45)
 
 
 
-# plt.show()
+plt.show()
 
 
 
 
 
 
-# fig, ax = plt.subplots()
-
-# fruits = ['apple', 'blueberry', 'cherry', 'orange']
-# counts = [40, 100, 30, 55]
-# bar_labels = ['red', 'blue', '_red', 'orange']
-# bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
-
-# ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
-
-# ax.set_ylabel('fruit supply')
-# ax.set_title('Fruit supply by kind and color')
-# ax.legend(title='Fruit color')
-
-# plt.show()
